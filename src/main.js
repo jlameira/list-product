d236087c4aeb09cde0d9a86b6b7e7a6e887115a0
Jonathan Lameira <c906947@pmmg.mg.gov.br>
2018-04-13 09:47:40 -0300

    adicionado lista

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-console */
+
 import Vue from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import { ClientTable, Event, ServerTable } from 'vue-tables-2';
+
+Vue.use(ClientTable);
+Vue.use(Event);
+Vue.use(ServerTable);
 
 Vue.config.productionTip = false;
 
