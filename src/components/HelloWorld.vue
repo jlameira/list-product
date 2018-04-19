ef42197c7d0af855bf891159056c9ef400312049
jlameira <jlameira@gmail.com>
2018-04-19 00:32:01 -0300

    adicionado pontuam mais

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,14 @@
 <template>
-<div>
+<div class="my-3 p-3 bg-white rounded box-shadow">
 
-   <v-client-table :data="tableData" :columns="columns" :options="options"></v-client-table>
+
+<div class="row">
+    <div class="lead">
+
+  <button  type="button" class="btn btn-outline-secondary" v-on:click="listaOsProdutosPrecoPontos" >Pontuam Mais</button>
+  </div>
+   <v-client-table :ref="100" :data="tableData" :columns="columns" :options="options"></v-client-table>
+</div>
 </div>
 </template>
 
@@ -16,9 +23,26 @@ export default {
       tableData: produtos,
       options: {
         filterByColumn: true,
+        perPage: 100,
         // see the options API
       },
     };
+  },
+  methods: {
+    listaOsProdutosPrecoPontos() {
+      produtos.sort((a, b) => {
+        const vA = a.pontos / a.valor;
+        const vB = b.pontos / b.valor;
+        // a.media = vA;
+        // b.media = vB;
+        if (vA > vB) {
+          return -1;
+        }
+        return 1;
+      });
+      this.tableData = produtos;
+      // this.tableData
+    },
   },
 };
 </script>
